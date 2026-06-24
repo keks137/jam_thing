8671f22c32e6f33c0cf1dced5ea558ae7c2c0e55
akamikado <manishvasireddy1234@gmail.com>
2026-06-24 11:37:23 +0530

    changes to build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ mkdir bin
 robocopy assets bin\assets /E
 set COMPILER_DIR=C:/raylib/w64devkit/bin
 set PATH=%PATH%;%COMPILER_DIR%
-cmd /c gcc src/unity.c -I C:/raylib/raylib/src -L C:/raylib/raylib/src -lraylib -lopengl32 -lgdi32 -lwinmm -lcomdlg32 -lole32 -o bin\jam_thing.exe && bin\jam_thing.exe
+cmd /c gcc src/unity.c -I vendor -I C:/raylib/raylib/src -L C:/raylib/raylib/src -lraylib -lopengl32 -lgdi32 -lwinmm -lcomdlg32 -lole32 --embed-file assets -o bin\jam_thing.exe && bin\jam_thing.exe
