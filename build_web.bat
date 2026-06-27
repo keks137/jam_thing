35969780c3f15ec79a86e1cd764b816cfb0dfd26
Dolphinman980 <130254302+Dolphinman980@users.noreply.github.com>
2026-06-27 14:50:59 +1000

    sfx

--- build_web.bat
+++ build_web.bat
@@ -2,5 +2,5 @@ REM call C:/raylib/emsdk/emsdk_env.bat
 mkdir bin
 robocopy assets bin\assets /E
 set NODE_SKIP_PLATFORM_CHECK=1
-C:/raylib/emsdk/upstream/emscripten/emcc -sINITIAL_MEMORY=83886080 --preload-file assets -sASSERTIONS -s FORCE_FILESYSTEM=1 -o bin/index.html src/unity.c -Os -Wall -Wextra -DPLATFORM_WEB -I c:/raylib/raylib/src c:/raylib/raylib/src/libraylib.web.a -I vendor -s USE_GLFW=3 --shell-file src/shell.html
+C:/raylib/emsdk/upstream/emscripten/emcc -sINITIAL_MEMORY=83886080 --preload-file assets -sASSERTIONS -s FORCE_FILESYSTEM=1 -sSTACK_SIZE=1mb -o bin/index.html src/unity.c -Os -Wall -Wextra -DPLATFORM_WEB -I c:/raylib/raylib/src c:/raylib/raylib/src/libraylib.web.a -I vendor -s USE_GLFW=3 --shell-file src/shell.html
 
--- src/main.c
+++ src/main.c
@@ -359,12 +359,12 @@ int main()
   srand(time(NULL));
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pizza Panic");
 
-  //InitAudioDevice();
+  InitAudioDevice();
 
-  //tossPizza = LoadSound(ORDER_OUT_SFX);
-  //newPizza = LoadSound(ORDER_IN_SFX);
-  //placeIngredient = LoadSound(PLACE_INGREDIENT_SFX);
-  //changeKnob = LoadSound(CHANGE_KNOB_SFX);
+  tossPizza = LoadSound(ORDER_OUT_SFX);
+  newPizza = LoadSound(ORDER_IN_SFX);
+  placeIngredient = LoadSound(PLACE_INGREDIENT_SFX);
+  changeKnob = LoadSound(CHANGE_KNOB_SFX);
 
   summer_font = LoadFont(SUMMER_FONT);
   cheese_font = LoadFontEx(CHEESE_FONT, 150, NULL, 0);
@@ -1084,7 +1084,7 @@ void PlaceToppingOnPizza(Vector2 mouse_pos) {
         .requested_position = TOPPING_POSITION_NONE
       }; 
       da_append(&pizzas.items[rotators[i].pizza_index].toppings, topping);
-      //PlaySound(placeIngredient);
+      PlaySound(placeIngredient);
     }
     topping_selected = TOPPING_NONE;
   }
@@ -1099,7 +1099,7 @@ void TossOrServe(Vector2 mouse_pos, double time) {
   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mouse_pos, leftTossButton)) {
     if (rotators[0].active) {
       pizzasTossed++;
-      //PlaySound(tossPizza);
+      PlaySound(tossPizza);
       pizzas.items[rotators[0].pizza_index].rotation = 0;
       pizzas.items[rotators[0].pizza_index].toppings.count = 0;
     }
@@ -1108,7 +1108,7 @@ void TossOrServe(Vector2 mouse_pos, double time) {
   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mouse_pos, rightTossButton)) {
     if (rotators[1].active) {
       pizzasTossed++;
-      //PlaySound(tossPizza);
+      PlaySound(tossPizza);
       pizzas.items[rotators[1].pizza_index].rotation = 0;
       pizzas.items[rotators[1].pizza_index].toppings.count = 0;
       
@@ -1118,7 +1118,7 @@ void TossOrServe(Vector2 mouse_pos, double time) {
   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mouse_pos, leftServeButton)) {
     if (rotators[0].active) {
       pizzasFinished++;
-      //PlaySound(newPizza);
+      PlaySound(newPizza);
       if (!firstPizzaServed) firstPizzaServed = true;
       rotators[0].spinning = false;
       rotators[0].active = false;
@@ -1132,7 +1132,7 @@ void TossOrServe(Vector2 mouse_pos, double time) {
   if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mouse_pos, rightServeButton)) {
     if (rotators[1].active) {
       pizzasFinished++;
-      //PlaySound(newPizza);
+      PlaySound(newPizza);
       if (!firstPizzaServed) firstPizzaServed = true;
       rotators[1].spinning = false;
       rotators[1].active = false;
